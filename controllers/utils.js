31b1f95715e7f8d259baa25d5bcfdab7ba9ea8e6
borel-T <borel-T@users.noreply.github.com>
2023-12-08 05:34:03 +0300

    edited refine util method

--- controllers/utils.js
+++ controllers/utils.js
@@ -1,7 +1,11 @@
 // util methods
 
 function refineProducts(data) {
-  return data.products.edges.map((item) => item.node);
+  return data.products.edges.map((item) => ({
+    id: item.node.id,
+    bodyHtml: item.node.bodyHtml,
+    images: item.node.images?.nodes,
+  }));
 }
 
 module.exports = {
